fcf40b51439905fe9cd606edf5126ea364e3d33a
ChristophGmeiner <christoph.gmeiner@gmail.com>
2020-02-23 12:41:28 +0100

    dag: setting defauls args

--- dags/udac_example_dag.py
+++ dags/udac_example_dag.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timedelta
+import configparser
 import os
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
@@ -6,12 +7,21 @@ from airflow.operators import (StageToRedshiftOperator, LoadFactOperator,
                                 LoadDimensionOperator, DataQualityOperator)
 from helpers import SqlQueries
 
-# AWS_KEY = os.environ.get('AWS_KEY')
-# AWS_SECRET = os.environ.get('AWS_SECRET')
+config = configparser.ConfigParser()
+config.read("dwh.cfg")
+
+os.environ['AWS_KEY'] = config["AWS"]["KEY"] 
+os.environ['AWS_SECRET'] = config["AWS"]["SECRET"]
 
 default_args = {
     'owner': 'udacity',
-    'start_date': datetime(2019, 1, 12),
+    'start_date': datetime.now(),
+    'catchup': False,
+    'retries': 3,
+    'retry_delay': timedelta(seconds=300),
+    'email_on_retry': False,
+    'depends_on_past': False
+    
 }
 
 dag = DAG('udac_example_dag',
@@ -63,3 +73,17 @@ run_quality_checks = DataQualityOperator(
 )
 
 end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
+
+start_operator >> stage_events_to_redshift
+start_operator >> stage_songs_to_redshift
+stage_events_to_redshift >> load_songplays_table
+stage_songs_to_redshift >> load_songplays_table
+load_songplays_table >> load_song_dimension_table
+load_songplays_table >> load_user_dimension_table
+load_songplays_table >> load_artist_dimension_table
+load_songplays_table >> load_time_dimension_table
+load_song_dimension_table >> run_quality_checks
+load_user_dimension_table >> run_quality_checks
+load_artist_dimension_table >> run_quality_checks
+load_time_dimension_table >> run_quality_checks
+run_quality_checks >> end_operator
